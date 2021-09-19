4d1dc1b96a3b48875e551a7b6c370291cf8d6643
Matt Worzala <mattheworzala@gmail.com>
2021-09-19 10:57:48 -0400

    add resource pack build & publish

--- resourcepack/bin/compile.sh
+++ resourcepack/bin/compile.sh
@@ -3,9 +3,20 @@
 # Move to `resourcepack` directory
 cd $( dirname $0 )/..
 
-# Create temp directory
-mkdir tmp
+# Create working directories
+mkdir canary_helper
+mkdir out
 
 # Copy src folder
+cp -r src/** canary_helper
+
+# todo replace version str
+
+# Compile resource pack
+echo "Compiling resource pack..."
+zip out/canary_helper.zip canary_helper
+
+# Cleanup
+rm -rf canary_helper
 
 echo $( pwd )
